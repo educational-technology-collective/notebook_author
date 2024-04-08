bbe8d59862c5773877bea76b6867fdfbeeb38412
Mengyan Wu <mengyanw@umich.edu>
2024-04-08 12:23:57 -0400

    Use postgres

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,44 @@
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [data, setData] = useState([])
+  const [assignments, setAssignments] = useState([])
+  const endpoint = import.meta.env.VITE_API_URL
+
+  const fetchData = async () => {
+    console.log('fetching...')
+    const response = await axios.get(endpoint)
+    console.log(response)
+    setAssignments(response.data) 
+    return response.data
+  }
+
+  const postData = async () => {
+    const description = 'test x'
+    const metadata_description = '123'
+    const stub = ''
+    const metadata_stub = ''
+    const body = {description, metadata_description, stub, metadata_stub}
+    const response = await axios.post(endpoint, body)
+    console.log(response)
+    return response.data
+  }
+
+  const handleSendData = async () => {
+    const newData = await postData()
+    setAssignments((prev) => [...prev, newData])
+  }
 
   useEffect(() => {
-    async function fetchData() {
-      console.log(import.meta.env.VITE_API_URL)
-      try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}`)
-        if (!response.ok) {
-          throw new Error('Network response error')
-        }
-        const result = response.json()
-        console.log(result)
-        setData(result)
-      }
-      catch (error) {
-        console.error('Error fetching data', error)
-      }
-    }
     fetchData()
   }, [])
 
   return (
     <>
-    Hello world
+      <ul>
+        {assignments.map(el => <li key={el.id}>{el.description}</li>)}
+      </ul>
+      <button onClick={handleSendData}>Add Assignment</button>
     </>
   )
 }
