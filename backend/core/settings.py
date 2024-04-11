a156a67ca8d890ef8cd56b98fbed8e7d48299228
Mengyan Wu <mengyanw@umich.edu>
2024-04-11 12:57:22 -0400

    Dockerize app

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         "NAME": "nbauthor",
         "USER": "mengyanw",
         "PASSWORD": "postgres",
-        "HOST": "127.0.0.1",
+        "HOST": "host.docker.internal",
         "PORT": "5432",
     }
 }
